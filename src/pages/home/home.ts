c4f918756e2e3b26cc58ae6a003028524a189744
Jose Cabrera <jose.cabrera@centrikal.com>
2017-03-31 09:16:59 -0600

    fix: remove unused code on home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,15 +2,6 @@ import {Component} from '@angular/core';
 
 import {NavController, Platform} from 'ionic-angular';
 
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  LatLng,
-  CameraPosition,
-  MarkerOptions,
-  Marker
-} from '@ionic-native/google-maps';
 import {GoogleMapsPage} from "../google-maps/google-maps";
 
 @Component({
@@ -21,60 +12,9 @@ export class HomePage {
 
   googleMapsPage = GoogleMapsPage;
 
-  constructor(public navCtrl: NavController,
-              private googleMaps: GoogleMaps,
-              private platform: Platform) {
-
-    // platform.ready().then(()=>{
-    //   console.log("PLATFORM READY!");
-    //   this.loadMap();
-    // });
+  constructor(public navCtrl: NavController) {
 
   }
 
-  loadMap() {
-    // make sure to create following structure in your view.html file
-    // and add a height (for example 100%) to it, else the map won't be visible
-    // <ion-content>
-    //  <div #map id="map" style="height:100%;"></div>
-    // </ion-content>
-
-    // create a new map by passing HTMLElement
-    let element: HTMLElement = document.getElementById('map');
-
-    let map: GoogleMap = this.googleMaps.create(element);
-
-    // listen to MAP_READY event
-    // You must wait for this event to fire before adding something to the map or modifying it in anyway
-    map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-      console.log('Map is ready!');
-
-      // create LatLng object
-      let ionic: LatLng = new LatLng(43.0741904, -89.3809802);
-
-      // create CameraPosition
-      let position: CameraPosition = {
-        target: ionic,
-        zoom: 18,
-        tilt: 30
-      };
-
-      // move the map's camera to position
-      map.moveCamera(position);
-
-      // create new marker
-      let markerOptions: MarkerOptions = {
-        position: ionic,
-        title: 'Ionic'
-      };
-
-      map.addMarker(markerOptions)
-        .then((marker: Marker) => {
-          marker.showInfoWindow();
-        });
-
-    });
-
-  }
 
 }
